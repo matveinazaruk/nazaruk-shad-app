306e2d24e041a5280ff8ee106cc63a4f34542844
Matvei Nazaruk <nazaruk@yandex-team.ru>
2018-01-30 22:54:08 +0300

    add profile and description

--- app/src/main/java/com/example/nazaruk/shad/MainActivity.java
+++ app/src/main/java/com/example/nazaruk/shad/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.nazaruk.shad;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
@@ -12,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        final ImageView avatarView = findViewById(R.id.avatar);
+        avatarView.setImageDrawable(roundImage(R.drawable.avatar));
+    }
+
+    private RoundedBitmapDrawable roundImage(final int avatar) {
+        final Bitmap avatarBitmap = BitmapFactory.decodeResource(getResources(), avatar);
+
+        final int length = Math.min(avatarBitmap.getWidth(), avatarBitmap.getHeight());
+        final Bitmap squareAvatar = Bitmap.createBitmap(avatarBitmap, 0, 0, length, length);
+
+        final RoundedBitmapDrawable roundedAvatarBitmap =
+                RoundedBitmapDrawableFactory.create(getResources(), squareAvatar);
+        roundedAvatarBitmap.setCircular(true);
+
+        return roundedAvatarBitmap;
     }
 }
